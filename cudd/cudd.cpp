edbc0a2c97c467f947d075512ab59e2a7f699c1d
Dragon116rus <rturbogda@yandex.ru>
2016-12-01 18:24:40 +0300

    without deref

--- cudd/cudd.cpp
+++ cudd/cudd.cpp
@@ -5,6 +5,7 @@
 #include "cudd.h"
 #include <iostream>
 #include <ctime>
+#include <sstream>
 using namespace std;
 
 DdManager * mgr;
@@ -14,8 +15,6 @@ int r=0;
 int n;
 void init() 
 {
-	cin>>n;
-	nodes1=new DdNode*[5*n];
 	mgr = Cudd_Init(n*n,0,CUDD_UNIQUE_SLOTS,CUDD_CACHE_SLOTS,0);
 	nodes =new DdNode**[n];
 	for (int i = 0; i < n; i++)
@@ -54,6 +53,7 @@ DdNode* getVerticalConj(int i,int k,int n)
 }
 DdNode* getVertical(int i,int n) 
 {
+
 	DdNode* root=Cudd_Not(Cudd_ReadOne(mgr));
 	Cudd_Ref(root);
 	for (int k = 0; k < n; k++)
@@ -182,7 +182,7 @@ DdNode* getDiagonalNEConj1(int i,int k,int n)
 		Cudd_Ref(conj);
 		Cudd_RecursiveDeref(mgr,root);
 		root=conj;
-		
+
 	}
 	return root;
 }
@@ -241,25 +241,32 @@ DdNode* buildBDD(int n)
 {
 	DdNode* root=Cudd_ReadOne(mgr);
 	Cudd_Ref(root);
+
+	unsigned int start_time =  clock();
+	unsigned int end_time = clock(); // конечное время
+	unsigned int search_time = end_time - start_time; 
+	DdNode** nodesToCon=new DdNode*[n];
 	for (int i = 0; i < n; i++)
 	{
+		
+
 		DdNode* vertical=getVertical(i,n);
-	//	Cudd_Ref(vertical);
 		DdNode* gorizontal=getGorizontal(i,n);
-	//	Cudd_Ref(gorizontal);
+
 		DdNode* diagonalNE1=getDiagonalNE1(i,n);
 		DdNode* diagonalNE2=getDiagonalNE2(i,n);
-	//	Cudd_Ref(diagonalNE);
+
 		DdNode* diagonalNW1=getDiagonalNW1(i,n);
 		DdNode* diagonalNW2=getDiagonalNW2(i,n);
-	//	Cudd_Ref(diagonalNW);
+
+
 
 		DdNode* lines=Cudd_bddAnd(mgr,gorizontal,vertical);
 		Cudd_Ref(lines);
 		Cudd_RecursiveDeref(mgr,gorizontal);
 		Cudd_RecursiveDeref(mgr,vertical);
 
-		nodes1[r++]=lines;
+
 
 		DdNode* diagonalNE=Cudd_bddAnd(mgr,diagonalNE1,diagonalNE2);
 		Cudd_Ref(diagonalNE);
@@ -276,52 +283,71 @@ DdNode* buildBDD(int n)
 		Cudd_RecursiveDeref(mgr,diagonalNE);
 		Cudd_RecursiveDeref(mgr,diagonalNW);
 
-		nodes1[r++]=diagonals;
+
 
 		DdNode* dotRoot=Cudd_bddAnd(mgr,lines,diagonals);
 		Cudd_Ref(dotRoot);
 		Cudd_RecursiveDeref(mgr,lines);
 		Cudd_RecursiveDeref(mgr,diagonals);
 
-		nodes1[r++]=dotRoot;
 
-		DdNode* tmp=Cudd_bddAnd(mgr,dotRoot,root);//DdNode* tmp=Cudd_bddAnd(mgr,lines,root);//
+		nodesToCon[i]=dotRoot;
+		/*DdNode* tmp=Cudd_bddAnd(mgr,dotRoot,root);
 		Cudd_Ref(tmp);
-		Cudd_RecursiveDeref(mgr,dotRoot);//Cudd_RecursiveDeref(mgr,lines);//
+		Cudd_RecursiveDeref(mgr,dotRoot);
 		Cudd_RecursiveDeref(mgr,root);
-		root=tmp;
+		root=tmp;*/
+
+		
 	}
+	int m=n;
+	while (m!=1) {
+		start_time =  clock();
+		for (int j = 0; j < (m+1)/2; j++)
+		{
+			int a=j;
+			int b=m-j-1;
+			if (a==b)
+				continue;
+			DdNode* tmp=Cudd_bddAnd(mgr,nodesToCon[a],nodesToCon[b]);
+			Cudd_Ref(tmp);
+			//Cudd_RecursiveDeref(mgr,nodesToCon[a]);
+			//Cudd_RecursiveDeref(mgr,nodesToCon[b]);
+			nodesToCon[a]=tmp;
+		}
+		m=(m+1)/2;
+		end_time = clock(); // конечное время
+		search_time = end_time - start_time; 
+		cout<<"tmp:"<<search_time<<endl;
+	}
+	root=nodesToCon[0];
 	return root;
 }
-int main()
+int main(int argc, char* argv[])
 {
-	 unsigned int start_time =  clock();
-	init();
+	stringstream ss; 
 
+	ss<<argv[1];
+	ss>>n;
+
+	unsigned int start_time =  clock();
+
+	//cin>>n;
+
+	init();
 	DdNode* root=buildBDD(n);
 
-	/* Build BDD. */
 
-	 unsigned int end_time = clock(); // конечное время
-    unsigned int search_time = end_time - start_time; 
+
+	unsigned int end_time = clock(); // конечное время
+	unsigned int search_time = end_time - start_time; 
 	cout<<search_time<<endl;
-	/* Inspect it. */
-	//  printf("f");
-	// Cudd_PrintSummary(mgr, f, 4, 0);
-	//Cudd_PrintDebug(mgr,f,1,4);
-	Cudd_bddPrintCover1(mgr, root, root,n);
 
-	// char * fform = Cudd_FactoredFormString(mgr, f, inames);
-	// printf("%s\n", fform);
-	/* Break up camp and go home. */
-	// free(fform);
-	// Cudd_RecursiveDeref(mgr, f);
-	//  int err = Cudd_CheckZeroRef(mgr);
-	// Cudd_Quit(mgr);
+	//Cudd_bddPrintCover1(mgr, root, root,n);
 
-	nodes1[0]=root;
-	FILE* file=fopen("out.txt","w");
-	Cudd_DumpDot(mgr,1,nodes1,NULL,NULL,file);
+
+
+
 	dispose();
 	return 0;
 }
